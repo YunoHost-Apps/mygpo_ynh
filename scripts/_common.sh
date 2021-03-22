4cf9a13d1d1d3d11b9a8fe7c517b182db5cd5ebd
Jules Bertholet <jules.bertholet@gmail.com>
2021-03-22 12:59:00 -0400

    Restrict permissions on app directories

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3 python3-pip python3-venv postgresql postgresql-contrib libpq-dev redis-server"
+pkg_dependencies="acl python3 python3-pip python3-venv postgresql postgresql-contrib libpq-dev redis-server"
 
 #=================================================
 # PERSONAL HELPERS
