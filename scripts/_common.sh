635368b11c5aeb6eba396ea3cb75c48216f5aff6
Jules Bertholet <jules.bertholet@gmail.com>
2021-04-17 12:21:01 -0400

    Improve packaging practices

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,6 +10,59 @@ pkg_dependencies="acl python3 python3-pip python3-venv postgresql postgresql-con
 # PERSONAL HELPERS
 #=================================================
 
+function set_permissions {
+	mkdir -p $data_path
+
+	env_path=$final_path/envs/prod
+	mkdir -p $env_path
+
+	chown -R $app:$app $data_path
+	chmod o-rwx $data_path
+	setfacl -n -R -m u:www-data:rx -m d:u:www-data:rx $data_path
+
+	chown -R root:$app $final_path
+	chmod -R g=u,g-w,o-rwx $final_path
+	setfacl -n -R -m user:www-data:rx -m default:user:www-data:rx $final_path
+	setfacl -n -R -m user:www-data:- -m default:user:www-data:- $final_path/envs
+}
+
+function set_up_virtualenv {
+	env_path=$final_path/envs/prod
+	mkdir -p $env_path
+
+	pushd $final_path || ynh_die
+		chown -R $app:$app $final_path
+		sudo -u $app python3 -m venv $final_path/venv
+		sudo -u $app $final_path/venv/bin/pip --cache-dir $final_path/.cache/pip install -U wheel pip --cache-dir $final_path/.cache/pip setuptools
+		sudo -u $app $final_path/venv/bin/pip --cache-dir $final_path/.cache/pip install -U --requirement $final_path/requirements.txt
+		sudo -u $app $final_path/venv/bin/pip --cache-dir $final_path/.cache/pip install -U --requirement $final_path/requirements-setup.txt
+		sudo -u $app $final_path/venv/bin/pip --cache-dir $final_path/.cache/pip install -U --requirement $final_path/requirements-ynh.txt
+		set_permissions
+	popd || ynh_dies
+}
+
+function initialize_db {
+	pushd $final_path || ynh_die
+		chown -R $app:$app $final_path
+		perform_db_migrations
+		sudo -u $app $final_path/venv/bin/envdir $env_path $final_path/venv/bin/python $final_path/manage.py createsuperuser --username "$admin" --email "$admin_email" --noinput -v 0
+		set_permissions
+	popd || ynh_die
+}
+
+function upgrade_db {
+	pushd $final_path || ynh_die
+		chown -R $app:$app $final_path
+		perform_db_migrations
+		set_permissions
+	popd || ynh_die
+}
+
+function perform_db_migrations {
+	sudo -u $app $final_path/venv/bin/envdir $env_path $final_path/venv/bin/python $final_path/manage.py makemigrations
+	sudo -u $app $final_path/venv/bin/envdir $env_path $final_path/venv/bin/python $final_path/manage.py migrate
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
